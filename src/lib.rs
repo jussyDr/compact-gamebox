22f9bf21ff39ba6e92f76959a831f0e4047ec464
Justin de Ruiter <justinderuiter@ziggo.nl>
2023-12-31 19:00:51 +0100

    Add basic no-op implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
 #![warn(missing_docs)]
 
-//! A more compact format for storing GameBox files.
+//! A compacted format for storing GameBox files.
+
+use std::io::{self, Read, Result, Write};
+
+/// Convert a GameBox file from the given `reader` to a compacted GameBox file at the given `writer`.
+pub fn compact(mut reader: impl Read, mut writer: impl Write) -> Result<()> {
+    io::copy(&mut reader, &mut writer)?;
+
+    Ok(())
+}
+
+/// Convert a compacted GameBox file from the given `reader` to a GameBox file at the given `writer`.
+pub fn decompact(mut reader: impl Read, mut writer: impl Write) -> Result<()> {
+    io::copy(&mut reader, &mut writer)?;
+
+    Ok(())
+}
